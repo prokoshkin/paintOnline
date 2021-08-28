ed8f602585319279cc3f802c448f5bdc28fc212a
Leonid <prokoshkinl@yandex.ru>
2021-08-29 00:48:18 +0300

    add tools for drowing

--- paint-online/src/components/Toolbar.js
+++ paint-online/src/components/Toolbar.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import canvasState from '../store/canvasState'
+import toolState from '../store/toolState'
 import '../styles/toolbar.scss'
+import Brush from '../tools/Brush'
+import Circle from '../tools/Circle'
+import Rect from '../tools/Rect'
 
 export const Toolbar = () => {
     return (
         <div className="toolbar"> 
-            <button className="toolbar__btn brush"/>
-            <button className="toolbar__btn brush"/>
-            <button className="toolbar__btn brush"/>
+            <button className="toolbar__btn brush" onClick={() => toolState.setTool(new Brush(canvasState.canvas))}/>
+            <button className="toolbar__btn rect" onClick={()=> 
+            toolState.setTool(new Rect(canvasState.canvas))}/>
+            <button className="toolbar__btn circle" onClick={()=> toolState.setTool(new Circle(canvasState.canvas))}/>
             <button className="toolbar__btn brush"/>
             <button className="toolbar__btn brush"/>
             <input style={{marginLeft: 10}} type="color"/>
